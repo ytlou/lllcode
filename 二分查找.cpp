f5e57be0e60e70d544d99594eed2bdd8430cfdb1
Tommy <36944406+ytlou@users.noreply.github.com>
2024-01-30 21:47:16 -0500

    Update 二分查找.cpp

--- 二分查找.cpp
+++ 二分查找.cpp
@@ -1,1 +1,94 @@
+//给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+//有序数组，同时题目还强调数组中无重复元素
+
+//对区间的定义没有想清楚，区间的定义就是不变量。要在二分查找的过程中，保持不变量，就是在while寻找中每一次边界的处理都要坚持根据区间的定义来操作，这就是循环不变量规则
+//左闭右闭即[left, right]，或者左闭右开即[left, right)
+//while (left <= right)
+//if (nums[middle] > target) right 要赋值为 middle - 1 大点 比大的更小找
+//if (nums[middle] < target) { left = middle + 1      小点 
+
+//while (left < right)
+//if (nums[middle] > target) right 要赋值为 middle 大点 比大的更小找
+//if (nums[middle] < target) { left = middle + 1      小点 
+
+//区别在于 一个等号 和右边界+1
+
+// 版本一
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1; // 定义target在左闭右闭的区间里，[left, right]
+        while (left <= right) { // 当left==right，区间[left, right]依然有效，所以用 <=
+            int middle = left + ((right - left) / 2);// 防止溢出 等同于(left + right)/2
+            if (nums[middle] > target) {
+                right = middle - 1; // target 在左区间，所以[left, middle - 1]
+            } else if (nums[middle] < target) {
+                left = middle + 1; // target 在右区间，所以[middle + 1, right]
+            } else { // nums[middle] == target
+                return middle; // 数组中找到目标值，直接返回下标
+            }
+        }
+        // 未找到目标值
+        return -1;
+    }
+};
+//int middle = left + ((right - left) / 2);// 防止溢出 等同于(left + right)/2
+//边界统一
+
+//logn
+
+
+
+
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+//class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+        // 分别处理如下三种情况
+        // 目标值在数组所有元素之前
+        // 目标值等于数组中某一个元素
+        // 目标值插入数组中的位置
+            if (nums[i] >= target) { // 一旦发现大于或者等于target的num[i]，那么i就是我们要的结果
+                return i;
+            }
+        }
+        // 目标值在数组所有元素之后的情况
+        return nums.size(); // 如果target是最大的，或者 nums为空，则返回nums的长度
+    }
+};  暴力法 n
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1; // 定义target在左闭右闭的区间里，[left, right]
+        while (left <= right) { // 当left==right，区间[left, right]依然有效
+            int middle = left + ((right - left) / 2);// 防止溢出 等同于(left + right)/2
+            if (nums[middle] > target) {
+                right = middle - 1; // target 在左区间，所以[left, middle - 1]
+            } else if (nums[middle] < target) {
+                left = middle + 1; // target 在右区间，所以[middle + 1, right]
+            } else { // nums[middle] == target
+                return middle;
+            }
+        }
+        // 分别处理如下四种情况
+        // 目标值在数组所有元素之前  [0, -1]
+        // 目标值等于数组中某一个元素  return middle;
+        // 目标值插入数组中的位置 [left, right]，return  right + 1 = left
+        // 目标值在数组所有元素之后的情况 [left, right]， 因为是右闭区间，所以 return right + 1
+        return right + 1;
+    }
+};
+
+
+//给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+
+
 
