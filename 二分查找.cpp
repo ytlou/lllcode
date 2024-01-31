194e85aaa02cc2c634a4da419ea311251058d423
Tommy <36944406+ytlou@users.noreply.github.com>
2024-01-31 18:11:32 -0500

    Update 二分查找.cpp

--- 二分查找.cpp
+++ 二分查找.cpp
@@ -60,7 +60,7 @@ public:
     }
 };  暴力法 n
 
-
+12 34
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
@@ -88,7 +88,122 @@ public:
 
 
 //给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+//bool check(int x) {/* ... */} // 检查x是否满足某种性质
+
+// 区间[l, r]被划分成[l, mid]和[mid + 1, r]时使用：
+int bsearch_1(int l, int r)
+{
+    while (l < r)
+    {
+        int mid = l + r >> 1;
+        if (check(mid)) r = mid;    // check()判断mid是否满足性质
+        else l = mid + 1;
+    }
+    return l;
+}
+// 区间[l, r]被划分成[l, mid - 1]和[mid, r]时使用：
+int bsearch_2(int l, int r)
+{
+    while (l < r)
+    {
+        int mid = l + r + 1 >> 1;
+        if (check(mid)) l = mid;
+        else r = mid - 1;
+    }
+    return l;
+}
+12  34
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(nums.size()==0)  return {-1,-1};
+        int l = 0;
+        int r = nums.size()-1;
+        while(l<r){
+            int mid = (l+r)>>1;
+            if(nums[mid]>=target) r = mid;
+            else l = mid +1;
+        }
+        if(nums[l]!=target) return {-1,-1};
+        else {
+            int l1 = l;
+            int l = 0;
+            int r = nums.size()-1;
+            while(l<r){
+                int mid = (l+r+1)>>1;// +++++1
+                if(nums[mid]<=target) l = mid;
+                else r = mid -1;
+            }
+            
+        return{l1,l};
+        }
+        return {-1,-1};}
+};
+
+唯一的 我们用 l<=r mid>target r = m - 1 else if mid<target l= m+1 else return
+            l< r            r = m
+
+非唯一的。   l<r  mid>=target r = m else l=mid +1; 左边界
+                mid<=target l = m else r = mid -1 右边界
 
 
 
+x 的平方根 3
+    class Solution {
+public:
+    bool check(long long mid, int x) {
+    return mid * mid > x;  // 注意这里的条件变为了 mid * mid > x
+}
 
+int mySqrt(int x) {
+    if (x == 0 || x == 1) {
+        return x;
+    }
+
+    int l = 1, r = x;
+    while (l < r) {
+        int mid = (l + r)>>1;  // 向下取整
+        if (check(mid, x)) {
+            r = mid;  // 如果mid不满足条件（即 mid * mid > x），向左侧搜索
+        } else {
+            l = mid + 1;  // 否则向右侧搜索
+        }
+    }
+    return l - 1;  // 返回 l - 1，因为我们需要的是最大的 n 使得 n^2 <= x
+}
+};
+
+小数 3 +4
+#include<iostream>
+#include<iomanip>
+using namespace std;
+double n,l,r,mid;
+bool flag;
+double q(double a){return a*a*a;}
+int main(){
+    cin>>n;
+    l=-10000,r=10000;
+    while(r-l>=1e-7){
+        mid=(l+r)/2;
+        if(q(mid)>=n) r=mid;
+        else l=mid;
+    }
+    cout<<fixed<<setprecision(6)<<l;
+    return 0;
+}
+
+
+唯一的 
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+            int l = 0, r= num;
+            while (l<=r){
+                int mid = (r-l)/2+l;
+                long square = (long )mid*mid;
+                if(square <num) l = mid+1;
+                else if(square >num) r= mid -1;
+                  else return true;           }
+   return false;}  1 + 2 num +1 longlong 
+    
+};
